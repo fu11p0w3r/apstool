c0adfd89396a7459c077d60399513e260005c5b0
Information security reseacher <62951354+fu11p0w3r@users.noreply.github.com>
2020-06-13 01:40:21 +0300

    changes

--- admfinder.py
+++ admfinder.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import argparse
-import os
+from os import system, name
 import requests
 from clint.textui import colored, puts
 
 
 def banner():
+    system('cls') if name == 'nt' else system('clear')
     puts(colored.white("""  
 ██████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗███████╗██╗███╗   ██╗██████╗ ███████╗██████╗   
 ██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗ 
@@ -13,7 +14,7 @@ def banner():
 ██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗ 
 ██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║ 
 ╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝ v0.1 
-This is a tool that helps you find the admin panel on the site     [Author]:fu11p0w3r """))
+Little tool for searching admin panel login forms                  [Author]: fu11p0w3r """))
 
 
 def check(query):
@@ -23,38 +24,38 @@ def check(query):
             try:
                 url = url.strip('\n')
                 req_status = requests.get(query + url, timeout=args.t).status_code
+            except KeyboardInterrupt:
+                puts(colored.yellow('[V]Byeeee!'))
+                exit(0)
             except requests.exceptions.ConnectionError:
                 continue
             except requests.exceptions.ReadTimeout:
-                puts(colored.yellow('  [ ! ] Connection timeout error, please set the -t argument to more'))
+                puts(colored.yellow('[!]Connection timeout, please set the timeout using the -t argument'))
                 continue
             if (req_status == 200):
-                puts(colored.green('  [200] {}'.format(query + url)))
+                puts(colored.green(f'[200] {query + url}'))
                 result.append(query + url)
             else:
-                puts(colored.red('  [{}] {}'.format(req_status, query + url)))
+                puts(colored.red(f'[{req_status}] {query + url}'))
     f.close()
 
 
 if __name__ == '__main__':
-    os.system('clear')
     banner()
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', type=str, metavar='url', help='Url address for scanning. For example: http://target.com')
     parser.add_argument('-t', type=float, metavar='sec', help='Timeout for your request. By default 0.5 seconds', default=0.5)
-
     args = parser.parse_args()
     if args.u is not None:
         result = []
         check(args.u)
         if len(result) != 0:
-            os.system('clear')
             banner()
             puts(colored.green('<><><><><><><><><><><><><><><><><><><><><><>'))
             for i in result:
-                puts(colored.green('  [FOUND] ' + i))
+                puts(colored.green('[FOUND] ' + i))
         else:
             exit(0)
     else:
-        puts(colored.red("  [Error] Please specify the url address to scan using the -u argument or use -h to call for help"))
+        puts(colored.red("[Error] Please specify the url address to scan using the -u argument or use -h to call for help"))
         exit(0)
