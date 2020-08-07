07b5f6e8e77d55ac46e86a83e99151c91b0fa662
Information security reseacher <62951354+fu11p0w3r@users.noreply.github.com>
2020-08-07 19:34:38 +0300

    Update apstool.py

--- apstool.py
+++ apstool.py
@@ -21,7 +21,7 @@ class AdminPanelSearchTool(object):
 
     def _banner(self):
         self._clear_console()
-        self._clintprint("""  
+        self._clintprint("""
                    _              _ 
     __ _ _ __  ___| |_ ___   ___ | |
    / _` | '_ \/ __| __/ _ \ / _ \| |
@@ -47,24 +47,36 @@ class AdminPanelSearchTool(object):
             puts(colored.white(text))
 
     async def _scan(self, url):
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False, enable_cleanup_closed=True),
-                                         requote_redirect_url=False, ) as session:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False),
+                                         requote_redirect_url=False) as session:
             try:
-                response = await session.get(url, allow_redirects=False)
+                async with session.get(url, allow_redirects=False) as response:
+                    self._scanned_dirs_count += 1
+                    if response.status == 200:
+                        self._status_200.append(url)
+                    elif response.status == (300 or 301 or 302):
+                        self.redirected.append(url)
+                        
             except aiohttp.ClientConnectorError:
-                self._clintprint('[Error] Connection error, trying next...', 'red')
+                self._clintprint('[ERROR] Connection error, trying next...', 'red')
+                
+            except aiohttp.InvalidURL:
+                self._clintprint('\n[ERROR] Incorrect url address, please enter a correct url!', 'red')
+                exit()
+                
+            except ConnectionResetError:
+                self._clintprint('\n[ERROR] The remote host has forcibly dropped the connection!', 'red')
+                exit()
+                
+            except aiohttp.ClientOSError:
+                self._clintprint('\n[ERROR] The remote host is no longer available!', 'red')
+                exit()
+                
             else:
-                self._scanned_dirs_count += 1
-                if response.status == 200:
-                    self._status_200.append(url)
-                elif response.status == (300 or 301 or 302):
-                    self.redirected.append(url)
-
                 self._banner()
-
                 self._print_current_founded()
-                #self._clintprint(' ')
-
+         
+                self._clintprint(' ')
                 self._bar.next()
                 self._clintprint(f' Scanning...')
                 self._clintprint(f'==> {url}', 'red')
@@ -92,8 +104,8 @@ class AdminPanelSearchTool(object):
         try:
             file = open(path, 'r')
         except FileNotFoundError:
-            self._clintprint('[ERROR] The list with locations was not found. \n'
-                             '        Please put the list in the root of this folder with the program', 'red')
+            self._clintprint('[ERROR] The wordlist with locations was not found. \n'
+                             '        Please put the wordlist in the root of this', 'red')
             exit()
         else:
             for line in file:
@@ -101,7 +113,9 @@ class AdminPanelSearchTool(object):
             file.flush()
             file.close()
             self._clintprint('\n[INFO] List with possible page locations loaded!')
+            time.sleep(1)
             self._clintprint('[INFO] Start scanning, please wait...')
+            time.sleep(1)
 
     def run(self):
         start_time = time.time()
@@ -116,19 +130,30 @@ class AdminPanelSearchTool(object):
             loop.run_until_complete(asyncio.gather(*tasks))
         except KeyboardInterrupt:
             self._banner()
-            self._clintprint('[CTRL+C] Byeeee!!!', 'yellow')
+            self.print_byee_and_exit()
+        except aiohttp.ClientOSError:
+            self._clintprint('[ERROR] The specified host is no longer available')
+            exit()
         else:
             self._bar.finish()
             total_elapsed_time = int(time.time() - start_time)
             self._print_total_info(total_elapsed_time)
 
-    def set_url(self, url):
-        self._url = url
+    def input_url(self):
+        try:
+            self._banner()
+            self._clintprint('Example: http://google.com')
+            self._url = str(input('[=>] Enter the full URL to start: '))
+        except KeyboardInterrupt:
+            self.print_byee_and_exit()
+
+    def print_byee_and_exit(self):
+        self._clintprint('\n[!] CTRL+C pressed, Byeeee!!!', 'yellow')
+        exit()
 
 
 if __name__ == '__main__':
     finder = AdminPanelSearchTool()
-    print('[Example] http://google.com')
-    finder.set_url(str(input('[=>] Enter the full URL to start: ')))
+    finder.input_url()
     finder.load_wordlist('wordlist.txt')
     finder.run()
